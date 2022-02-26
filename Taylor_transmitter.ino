ae478a49aa8ccbf6a3bccd7fdba60ffe9381d550
armisteadab <52898845+armisteadab@users.noreply.github.com>
2022-02-25 21:22:46 -0500

    Update Taylor_transmitter.ino
    replaced 0 and 1 w H + L, fixed problem with what signal to send

--- Taylor_transmitter.ino
+++ Taylor_transmitter.ino
@@ -20,26 +20,26 @@ const int switch7 = 32;
 const int switch8 = 33;
 const int switch9 = 34;
 const int switch10 = 35;
-char switch_position1 = "0";
-char switch_position1_previous = "0";
-char switch_position2 = "0";
-char switch_position2_previous = "0";
-char switch_position3 = "0";
-char switch_position3_previous = "0";
-char switch_position4 = "0";
-char switch_position4_previous = "0";
-char switch_position5 = "0";
-char switch_position5_previous = "0";
-char switch_position6 = "0";
-char switch_position6_previous = "0";
-char switch_position7 = "0";
-char switch_position7_previous = "0";
-char switch_position8 = "0";
-char switch_position8_previous = "0";
-char switch_position9 = "0";
-char switch_position9_previous = "0";
-char switch_position10 = "0";
-char switch_position10_previous = "0";
+char switch_position1 = "L";
+char switch_position1_previous = "L";
+char switch_position2 = "L";
+char switch_position2_previous = "L";
+char switch_position3 = "L";
+char switch_position3_previous = "L";
+char switch_position4 = "L";
+char switch_position4_previous = "L";
+char switch_position5 = "L";
+char switch_position5_previous = "L";
+char switch_position6 = "L";
+char switch_position6_previous = "L";
+char switch_position7 = "L";
+char switch_position7_previous = "L";
+char switch_position8 = "L";
+char switch_position8_previous = "L";
+char switch_position9 = "L";
+char switch_position9_previous = "L";
+char switch_position10 = "L";
+char switch_position10_previous = "L";
 bool firstTime = true;
 
 void setup()
@@ -66,16 +66,16 @@ void setup()
   pinMode(switch9, INPUT);
   pinMode(switch10, INPUT);
 
-   switch_position1="0"; // clear
-   switch_position2="0"; // clear
-   switch_position3="0"; // clear
-   switch_position4="0"; // clear
-   switch_position5="0"; // clear
-   switch_position6="0"; // clear
-   switch_position7="0"; // clear
-   switch_position8="0"; // clear
-   switch_position9="0"; // clear
-   switch_position10="0"; // clear
+   switch_position1="L"; // clear
+   switch_position2="L"; // clear
+   switch_position3="L"; // clear
+   switch_position4="L"; // clear
+   switch_position5="L"; // clear
+   switch_position6="L"; // clear
+   switch_position7="L"; // clear
+   switch_position8="L"; // clear
+   switch_position9="L"; // clear
+   switch_position10="L"; // clear
 
 }
 
@@ -84,55 +84,55 @@ void loop()
 
 
   if (digitalRead(switch1) == HIGH) {
-    switch_position1 = "1";
+    switch_position1 = "H";
     } else {
-    switch_position1 = "0";      
+    switch_position1 = "L";      
     }
     
   if (digitalRead(switch2) == HIGH) {
-    switch_position2 = "1";
+    switch_position2 = "H";
     } else {
-    switch_position2 = "0";      
+    switch_position2 = "L";      
     }
   if (digitalRead(switch3) == HIGH) {
-    switch_position3 = "1";
+    switch_position3 = "H";
     } else {
-    switch_position3 = "0";      
+    switch_position3 = "L";      
     }
   if (digitalRead(switch4) == HIGH) {
-    switch_position4 = "1";
+    switch_position4 = "H";
     } else {
-    switch_position4 = "0";      
+    switch_position4 = "L";      
     }
   if (digitalRead(switch5) == HIGH) {
-    switch_position5 = "1";
+    switch_position5 = "H";
     } else {
-    switch_position5 = "0";      
+    switch_position5 = "L";      
     }
   if (digitalRead(switch6) == HIGH) {
-    switch_position6 = "1";
+    switch_position6 = "H";
     } else {
-    switch_position6 = "0";      
+    switch_position6 = "L";      
     }
   if (digitalRead(switch7) == HIGH) {
-    switch_position7 = "1";
+    switch_position7 = "H";
     } else {
-    switch_position7 = "0";      
+    switch_position7 = "L";      
     }
   if (digitalRead(switch8) == HIGH) {
-    switch_position8 = "1";
+    switch_position8 = "H";
     } else {
-    switch_position8 = "0";      
+    switch_position8 = "L";      
     }
   if (digitalRead(switch9) == HIGH) {
-    switch_position9 = "1";
+    switch_position9 = "H";
     } else {
-    switch_position9 = "0";      
+    switch_position9 = "L";      
     }
   if (digitalRead(switch10) == HIGH) {
-    switch_position10 = "1";
+    switch_position10 = "H";
     } else {
-    switch_position10 = "0";      
+    switch_position10 = "L";      
     }
 
   // don't switch anything the first time thru, since we're just getting/saving the values
@@ -141,110 +141,120 @@ void loop()
       
 
         if (switch_position1 != switch_position1_previous){
-        char *msg = "RRSW1";    
-         *msg += switch_position1;
-         *msg += "X";
-         *msg += switch_position1_previous;
+            char *msg = "RRSW1L";
+            if (switch_position1 == HIGH){
+               *msg = "RRSW1H";
+            }
     
         driver.send((uint8_t *)msg, strlen(msg));
         driver.waitPacketSent();
-        Serial.println("switch 1");
         delay(200);      
           }
     
     
         if (switch_position2 != switch_position2_previous){
-        char *msg = "RRSW2";
-         *msg += switch_position2;
-         *msg += "X";
-         *msg += switch_position2_previous;
-    
+            char *msg = "RRSW2L";
+            if (switch_position2 == HIGH){
+               *msg = "RRSW2H";
+            }
         driver.send((uint8_t *)msg, strlen(msg));
         driver.waitPacketSent();
-        Serial.println("switch 2");
         delay(200);      
           }
     
     
         if (switch_position3 != switch_position3_previous){
-        char *msg = "RRSW3";
-         *msg += switch_position3;
-         *msg += "X";
-         *msg += switch_position3_previous;
-    
+           char *msg = "RRSW3L";
+            if (switch_position3 == HIGH){
+               *msg = "RRSW3H";
+            }
+     
         driver.send((uint8_t *)msg, strlen(msg));
         driver.waitPacketSent();
-        Serial.println("switch 3");
         delay(200);      
           }
     
     
         if (switch_position4 != switch_position4_previous){
-        char *msg = "RRSW4";
-    
+           char *msg = "RRSW4L";
+            if (switch_position4 == HIGH){
+               *msg = "RRSW4H";
+            }
+     
         driver.send((uint8_t *)msg, strlen(msg));
         driver.waitPacketSent();
-        Serial.println("switch 4");
         delay(200);      
           }
     
     
         if (switch_position5 != switch_position5_previous){
-        char *msg = "RRSW5";
-    
+           char *msg = "RRSW5L";
+            if (switch_position5 == HIGH){
+               *msg = "RRSW5H";
+            }
+     
         driver.send((uint8_t *)msg, strlen(msg));
         driver.waitPacketSent();
-        Serial.println("switch 5");
         delay(200);      
           }
     
         if (switch_position6 != switch_position6_previous){
-        char *msg = "RRSW6";
-    
+           char *msg = "RRSW6L";
+            if (switch_position6 == HIGH){
+               *msg = "RRSW6H";
+            }
+     
         driver.send((uint8_t *)msg, strlen(msg));
         driver.waitPacketSent();
         delay(200);      
-         Serial.println("switch 6");
          }
     
     
         if (switch_position7 != switch_position7_previous){
-        char *msg = ("RRSW7");
-    
+           char *msg = "RRSW7L";
+            if (switch_position7 == HIGH){
+               *msg = "RRSW7H";
+            }
+     
         driver.send((uint8_t *)msg, strlen(msg));
         driver.waitPacketSent();
         delay(200);      
-        Serial.println("switch 7");
           }
     
     
         if (switch_position8 != switch_position8_previous){
-        char *msg = "RRSW8";
-    
+           char *msg = "RRSW8L";
+            if (switch_position8 == HIGH){
+               *msg = "RRSW8H";
+            }
+     
         driver.send((uint8_t *)msg, strlen(msg));
         driver.waitPacketSent();
         delay(200);      
-            Serial.println("switch 8");
     
           }
     
     
         if (switch_position9 != switch_position9_previous){
-        char *msg = ("RRSWITCH9");
-    
+           char *msg = "RRSW9L";
+            if (switch_position9 == HIGH){
+               *msg = "RRSW9H";
+            }
+     
         driver.send((uint8_t *)msg, strlen(msg));
         driver.waitPacketSent();
-        Serial.println("switch 9");
         delay(200);      
           }
     
     
         if (switch_position10 != switch_position10_previous){
-        char *msg = "RRSW10";
-    
+           char *msg = "RRSW10L";
+            if (switch_position10 == HIGH){
+               *msg = "RRSW10H";
+            }
+     
         driver.send((uint8_t *)msg, strlen(msg));
         driver.waitPacketSent();
-        Serial.println("switch 10");
         delay(200);      
           }
     }
